c418f42adf46b3d47d80f02f540fc554da2fbcbb
LaitonGarcia <laitongarcia@hotmail.com>
2017-05-01 01:29:21 -0300

    Add files via upload

---
+++ SensorPresencaAlarme.ino
@@ -0,0 +1,22 @@
+int pinSensor = 8;
+int valorSensor = 0;
+char saidaSerial = 'O';
+void setup() {
+  Serial.begin(9600); //Inicializando o serial monitor
+ 
+  //Definido pinos como de entrada ou de saída
+  pinMode(pinSensor,INPUT);
+
+}
+
+void loop() {
+  
+  valorSensor = digitalRead(pinSensor);
+  Serial.println(saidaSerial);
+
+  if (valorSensor == 1) {
+    saidaSerial = 'X';
+  } else {
+    saidaSerial = 'O';
+  }
+}
